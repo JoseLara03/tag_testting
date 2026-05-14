24d7a71b4dd4d7122bc8e54d9b8c1fe42cd725aa
Jose Lara <jose.lara@innovaforce.com.mx>
2026-05-14 12:49:56 -0600

    feat: add lis2hh12_if module (Zephyr I2C platform callbacks)

---
+++ src/lis2hh12_if.c
@@ -0,0 +1,35 @@
+#include <zephyr/device.h>
+#include <zephyr/drivers/i2c.h>
+#include "lis2hh12_if.h"
+
+#define LIS2HH12_ADDR 0x1EU  /* SA0 = GND */
+
+static int32_t platform_write(void *handle, uint8_t reg,
+                               const uint8_t *buf, uint16_t len)
+{
+    return i2c_burst_write((const struct device *)handle,
+                           LIS2HH12_ADDR, reg, buf, len);
+}
+
+static int32_t platform_read(void *handle, uint8_t reg,
+                              uint8_t *buf, uint16_t len)
+{
+    return i2c_burst_read((const struct device *)handle,
+                          LIS2HH12_ADDR, reg, buf, len);
+}
+
+int lis2hh12_if_init(stmdev_ctx_t *ctx)
+{
+    static const struct device *i2c_bus =
+        DEVICE_DT_GET(DT_NODELABEL(i2c0));
+
+    if (!device_is_ready(i2c_bus)) {
+        return -ENODEV;
+    }
+
+    ctx->write_reg = platform_write;
+    ctx->read_reg  = platform_read;
+    ctx->handle    = (void *)i2c_bus;
+
+    return 0;
+}
---
+++ src/lis2hh12_if.h
@@ -0,0 +1,8 @@
+#ifndef LIS2HH12_IF_H
+#define LIS2HH12_IF_H
+
+#include "lis2hh12_reg.h"
+
+int lis2hh12_if_init(stmdev_ctx_t *ctx);
+
+#endif /* LIS2HH12_IF_H */
